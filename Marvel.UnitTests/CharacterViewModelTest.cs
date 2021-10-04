c873579400b03c914a70b4d405b5644b709101fc
hfmorris8801@gmail.com <hfmorris8801@gmail.com>
2021-10-04 15:07:18 -0800

    Enabled Unit Testing

---
+++ Marvel.UnitTests/CharacterViewModelTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Marvel.UnitTests
+{
+    //UNO does not support Unit Test, but this an idea how it works 
+    [TestClass]
+    public class CharacterViewModelTest
+    {
+        [TestMethod]
+        public void Search_InvalidNumber_ReturnsNull()
+        {
+            //Arrange
+            //int characterId = 121212;
+            //var characterViewModel = new CharacterViewModel();
+
+            //Act
+            //characterViewModel.CharacterId = characterId;
+            //characterViewModel.ExecuteSearch().Execute(null);
+            //Assert
+            //Assert.IsNull(characterViewModel.Hero);
+        }
+        
+        [TestMethod]
+        public void Search_ValidNumber_ReturnsHero()
+        {
+            //Arrange
+            //int characterId = 1009610;
+            //var characterViewModel = new CharacterViewModel();
+
+            //Act
+            //characterViewModel.CharacterId = characterId;
+            //characterViewModel.ExecuteSearch().Execute(null);
+            //Assert
+            //Assert.IsNotNull(characterViewModel.Hero);
+        }
+    }
+}
---
+++ Marvel.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Marvel.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Marvel.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f56df1fe-bcae-4160-b745-52297aa66a63")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
